8d7e654d64a6dc51ee815801452c8c03294b1601
Msimboit <msimboit@gmail.com>
2021-11-04 12:40:20 +0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Routes } from "react-router-dom"
+import { HashRouter as Routes } from "react-router-dom"
 
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer"
